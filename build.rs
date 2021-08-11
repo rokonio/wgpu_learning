f9d6069d89089e5049a37ce93cdddf472fe3965f
rokonio <naelkanada@gmail.com>
2021-08-11 15:55:25 +0200

    Update build file for multiple shaders

--- build.rs
+++ build.rs
@@ -1,8 +1,14 @@
 use naga::front::wgsl::Parser;
+use std::fs;
+use std::io::prelude::*;
 
 fn main() {
+    let shaders_files = fs::read_dir("src/shaders").unwrap();
     let mut parser = Parser::new();
-    parser
-        .parse(include_str!("src/shaders/shader.wgsl"))
-        .unwrap();
+    for file in shaders_files {
+        let mut file = fs::File::open(file.unwrap().path()).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+        parser.parse(content.as_str()).unwrap();
+    }
 }
